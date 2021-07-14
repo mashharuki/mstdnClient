733fdb1a877b72706ac81a613eaa81e522a1b58f
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-07-14 18:50:36 +0900

    画面が正常に表示されるように修正しました！

--- main.js
+++ main.js
@@ -13,20 +13,25 @@ const BrowserWindow = electron.BrowserWindow
 // Electronのライフサイクル
 let mainWindow
 app.on('ready', createWindow)
-app.on('window-all-closed', () => app.quit())
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
 app.on('activate', () => {
     if (mainWindow === null) createWindow()
 })
 
 // ウィンドウを作成
 function createWindow () {
-    mainWindow = new BrowserWindow({width: 600, height: 800})
+    mainWindow = new BrowserWindow({
+        width: 600, 
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
     // index.htmlを読み込む
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+    mainWindow.loadURL('file://' + __dirname + '/index.html')
     mainWindow.on('closed', function () {
         mainWindow = null
     })
